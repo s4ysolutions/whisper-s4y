9caf68be5599f0a0dc64326a05a30c7ccf9b2166
Sergey Dolin <sergey@s4y.solutions>
2024-07-28 10:29:48 +0200

    try converter.optimizations = [tf.lite.Optimize.DEFAULT]
    Fixes crash but wrong answer

--- whisper2tflite.py
+++ whisper2tflite.py
@@ -81,9 +81,9 @@ if not skip_convert:
 #        tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
 #        tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
 #    ]
-#    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    converter.inference_input_type = tf.float32
-    converter.inference_output_type = tf.float32
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+#    converter.inference_input_type = tf.float32
+#    converter.inference_output_type = tf.float32
     # And now we have tflite model
     tflite_model = converter.convert()
 
